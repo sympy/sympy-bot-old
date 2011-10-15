99c8e8ee6e74fe17faf68feb50f31670ca0a1174
Ondrej Certik <ondrej@certik.cz>
2011-10-15 00:29:55 -0700

    Fix a typo

--- web/app/models.py
+++ web/app/models.py
@@ -4,7 +4,7 @@ class User(db.Model):
     login = db.StringProperty(required=True)
     id = db.IntegerProperty()
     url = db.StringProperty()
-    gravatar_url = db.StringProperty()
+    avatar_url = db.StringProperty()
     name = db.StringProperty()
     email = db.StringProperty()
 
