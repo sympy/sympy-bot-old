47712ee922f8fffe18e6cb38f468f5e588492fc3
Stefan Krastanov <krastanov.stefan@gmail.com>
2011-12-13 19:27:28 +0100

    Checks for new tests just in time
    Checks for new tests just before starting the tests instead of
    when building the list of PRs. That way the chance for duplicates
    is lower.
    
    Used like this for about a week and no duplicates were created.

--- workaround.py
+++ workaround.py
@@ -1,45 +1,46 @@
-from subprocess import Popen
+from subprocess import Popen, PIPE
 from time import sleep
 import re
 from urllib import urlopen
 from json import load
 
 
-############################################################################################################
+##############################################################################
 def really_ugly_solution_for_finding_the_last_test_sha(pr):
     html_link = pr['html_url']
     page = ''.join(urlopen(html_link).readlines())
     search = re.findall(r'<em>branch hash</em>: <a href="https://github.com/sympy/sympy/commit/(\w*)"', page)
     if search:
         return search[-1]
-############################################################################################################
+##############################################################################
 
 while True:
+    try:
+        open_pr = load(urlopen('https://api.github.com/repos/sympy/sympy/pulls'))
+        for pr in open_pr:
+            more_stuff = load(urlopen(pr['url']))
+            pr.update(more_stuff)
+        print "Open PRs:"
+        print [pr['number'] for pr in open_pr]
 
-    open_pr = load(urlopen('https://api.github.com/repos/sympy/sympy/pulls'))
-    for pr in open_pr:
-        more_stuff = load(urlopen(pr['url']))
-        pr.update(more_stuff)
-    print "OPEN PRs"
-    print [pr['number'] for pr in open_pr]
+        lambd_filt = lambda pr : pr['head']['sha'] != really_ugly_solution_for_finding_the_last_test_sha(pr)
 
-    lambd_filt = lambda pr : pr['head']['sha'] != really_ugly_solution_for_finding_the_last_test_sha(pr)
-    to_be_tested = filter(lambd_filt, open_pr)
-
-    print "TO BE TESTED"
-    print [pr['number'] for pr in to_be_tested]
-    for pr in to_be_tested:
-        number = str(pr['number'])
-        print 'Testing: ' + number
-        command = ['./sympy-bot', 'review']
-        command.append(number)
-        Popen(command).wait()
-        if pr['mergeable']:
-            command = ['./sympy-bot', '-3', 'review']
-            command.append(number)
-            Popen(command).wait()
+        for pr in open_pr:
+            if not lambd_filt(pr):
+                continue
+            number = str(pr['number'])
+            print 'Testing: ' + number
             command = ['./sympy-bot', '-i', 'python2.5', 'review']
             command.append(number)
             Popen(command).wait()
+            if pr['mergeable']:
+                command = ['./sympy-bot', '-3', 'review']
+                command.append(number)
+                Popen(command).wait()
+            #Popen('rm', '-rf', '/tmp/tmp*').wait()
+            #Popen('rm', '-rf', '/tmp/sympy*').wait()
 
-    sleep(3600)
+        print 'Sleeping...'
+        #sleep(3600)
+    except Exception:
+        continue
